a4b6d49db2290d055bfa4e0325ceb83caea3267a
Shad0wRim <jungouicha@gmail.com>
2024-12-01 22:15:35 -0600

    added main running structure

--- aoc2024/src/main.rs
+++ aoc2024/src/main.rs
@@ -1,3 +1,47 @@
+mod days;
+use days::*;
+
+const INTERACTIVE: bool = false;
+const DAY: i32 = 1;
+
 fn main() {
-    println!("Advent of Code 2024");
+    println!("Advent of Code 2024\n");
+
+    let day = if INTERACTIVE {
+        use std::io;
+        let mut buf = String::new();
+        print!("Input day to run: ");
+        io::stdin().read_line(&mut buf).unwrap();
+        buf.parse().unwrap_or(DAY)
+    } else {
+        DAY
+    };
+
+    match day {
+        1 => day1(),
+        2 => day2(),
+        3 => day3(),
+        4 => day4(),
+        5 => day5(),
+        6 => day6(),
+        7 => day7(),
+        8 => day8(),
+        9 => day9(),
+        10 => day10(),
+        11 => day11(),
+        12 => day12(),
+        13 => day13(),
+        14 => day14(),
+        15 => day15(),
+        16 => day16(),
+        17 => day17(),
+        18 => day18(),
+        19 => day19(),
+        20 => day20(),
+        21 => day21(),
+        22 => day22(),
+        23 => day23(),
+        24 => day24(),
+        _ => panic!(),
+    }
 }
