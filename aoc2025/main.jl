f7c882acbde972080173cebec8638ab290f7ff61
Shad0wRim <jungouicha@gmail.com>
2025-10-16 14:40:07 -0500

    formatted main

--- aoc2025/main.jl
+++ aoc2025/main.jl
@@ -3,7 +3,7 @@ using .aoc_days: aoc_day
 
 function main()
     currday = 1
-    datapath = "res/day$(lpad(currday,2, "0")).txt"
+    datapath = "res/day$(lpad(currday,2,"0")).txt"
     if !isfile(datapath) || stat(datapath).size == 0
         println("fetching puzzle data")
         data = get_aoc_data(currday)
@@ -36,7 +36,10 @@ end
 using HTTP
 function get_aoc_data(day::Int)
     cookie = ENV["AOC_TOKEN"]
-    header = ["Content-Type" => "text/plain", "cookie" => "session=$cookie"]
+    header = [
+        "Content-Type" => "text/plain",
+        "cookie" => "session=$cookie",
+    ]
     url = "https://adventofcode.com/2025/day/$day/input"
     response = try
         response = HTTP.get(url, header)
