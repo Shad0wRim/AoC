cf15499d0efb1a45a0f3b1f376d1b3af1a195f0f
Shad0wRim <jungouicha@gmail.com>
2025-12-07 00:10:25 -0600

    fixed error when only one arg is passed

--- 2025/run.jl
+++ 2025/run.jl
@@ -5,4 +5,4 @@ include("utils.jl")
 using .aoc_days: aoc_day
 using .utils
 
-runday(parse(Int, ARGS[1]), ARGS[2] == "practice")
+runday(parse(Int, ARGS[1]), length(ARGS) > 1 && ARGS[2] == "practice")
